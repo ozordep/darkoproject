8998481be7f3a21c5bf6d140f8648ba718144d47
jpped <jpped@DESKTOP-KDT2NAF>
2019-05-20 14:10:28 -0300

    Criação dos pacotes dao e jdbc

--- Documents/NetBeansProjects/DarkoProject/src/br/com/controller/LoginController.java
+++ Documents/NetBeansProjects/DarkoProject/src/br/com/controller/LoginController.java
@@ -18,15 +18,6 @@ import javafx.scene.control.Label;
  */
 public class LoginController implements Initializable {
 
-    @FXML
-    private Label label;
-
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
